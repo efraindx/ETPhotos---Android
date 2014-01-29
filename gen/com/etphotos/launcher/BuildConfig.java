5995926fa5d1d06245bea4906ca481f42167e71c
Efrain Toribio Reyes <efrainreyes05@hotmail.com>
2014-01-29 10:18:50 +0100

    uploading project..

---
+++ gen/com/etphotos/launcher/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.etphotos.launcher;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/etphotos/launcher/MainFragment.java
@@ -0,0 +1,94 @@
+package com.etphotos.launcher;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+	
+	private static final String TAG = "MainFragment";
+	private UiLifecycleHelper uiHelper;
+	
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		@Override
+		public void call(Session session, SessionState state, Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
+		
+	};
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.main, container, false);
+		
+	/*	LoginButton loginButton = (LoginButton) view.findViewById(R.id.authButton);
+		loginButton.setFragment(this);*/
+		return view;
+	}
+	
+	private void onSessionStateChange(Session session, SessionState state, Exception ex) {
+		if (state.isOpened()) {
+			Log.i(TAG, "Logged in...");
+		} else if (state.isClosed()) {
+			Log.i(TAG, "Logged out...");
+		}
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+		uiHelper.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		//For scenarios where the main acitivy is launched
+		//and user session is or not null, the session state
+		//change notification may not be triggered. here it
+		//is trigged
+		Session session = Session.getActiveSession();
+		if (session != null &&
+				(session.isOpened() || session.isClosed()) ) {
+			onSessionStateChange(session, session.getState(), null);
+		}
+		uiHelper.onResume();
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		uiHelper.onPause();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		uiHelper.onSaveInstanceState(outState);
+	}
+
+}
